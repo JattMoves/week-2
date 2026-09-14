57ce1ebd0cf414cc2212289762c82d644af583ba
Jattmoves <romanjeet10@gmail.com>
2026-09-13 21:39:34 -0400

    attempt1

--- apputil.py
+++ apputil.py
@@ -1,13 +1,29 @@
 import numpy as np
 
-
+names = np.array(['Hannah', 'Astrid', 'Abdul', 'Mauve', 'Jung'])
+scores = np.array([99, 71, 85, 62, 91])
 # update/add code below ...
 
 def ways(n):
-    return None
+    num_pennies = 1
+    num_nickels = 5
+
+    total_ways = 0
+    for pennies in range(n // num_pennies + 1):
+        for nickels in range(n // num_nickels + 1):
+            if pennies * num_pennies + nickels * num_nickels == n:
+                total_ways += 1
+                snap = (([pennies, nickels]))
+                print(snap)
+    return (total_ways)
 
 def lowest_score(names, scores):
-    return None
+    min_score = np.min(scores)
+    min_index = np.argmin(scores)
+    return names[min_index], min_score
 
 def sort_names(names, scores):
-    return None
+    sorted_indices = np.argsort(scores)
+    sorted_names = names[sorted_indices]
+    sorted_scores = scores[sorted_indices]
+    return sorted_names, sorted_scores
